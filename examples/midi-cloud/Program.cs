ee3768e0df02b4b85d02654a7f850df26354511b
Pieter Venter <dodgyrabbit@hotmail.com>
2020-12-28 19:39:21 -0500

    Add logic to upload to GBQ

--- examples/midi-cloud/Program.cs
+++ examples/midi-cloud/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Dodgyrabbit.Google.Cloud.PubSub.V1;
+using Google.Cloud.PubSub.V1;
+using System.Text.Json.Serialization;
+using Google.Cloud.BigQuery.V2;
+using midi_filter;
 
 namespace midi_cloud
 {
@@ -12,15 +19,56 @@ namespace midi_cloud
         
         static async Task Main(string[] args)
         {
-            await PublishAsync();
+            // await PublishAsync();
+            PullAndUploadToGBQ("", "", "", "");
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
+        
+        static int PullAndUploadToGBQ(string projectId, string subscriptionId, string datasetId, string tableId)
+        {
+            SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+            SubscriberServiceApiClient subscriber = SubscriberServiceApiClient.Create();
+
+            BigQueryClient client = BigQueryClient.Create(projectId);
+            ConcurrentBag<BigQueryInsertRow> rows = new ConcurrentBag<BigQueryInsertRow>();
+            
+            // Pull messages from the subscription. We're returning immediately, whether or not there
+            // are messages; in other cases you'll want to allow the call to wait until a message arrives.
+            PullResponse response = subscriber.Pull(subscriptionName, returnImmediately: true, maxMessages: Int32.MaxValue);
+            foreach (ReceivedMessage received in response.ReceivedMessages)
+            {
+                PubsubMessage message = received.Message;
+                string text = Encoding.UTF8.GetString(message.Data.ToByteArray());
+                
+                NoteMidiEvent note = System.Text.Json.JsonSerializer.Deserialize<NoteMidiEvent>(text);
+                
+                Console.WriteLine($"Message {message.MessageId}: {text}");
+                // The insert ID is optional, but can avoid duplicate data when retrying inserts.
+                rows.Add(new BigQueryInsertRow()
+                {
+                    {"timestamp", note.DateTime},
+                    {"device", "1"},
+                    {"type", 1},
+                    {"name", note.SPN},
+                    {"value", (int) note.Note},
+                    {"velocity", (int) note.Velocity},
+                    {"noteon", note.IsNoteOn}
+                });
+            }
+
+            if (rows.Count > 0)
+            {
+                client.InsertRows(datasetId, tableId, rows);
+                subscriber.Acknowledge(subscriptionName, response.ReceivedMessages.Select(m => m.AckId));
+            }
+            return rows.Count;
+        }
 
         static async Task PublishAsync()
         {
-            PublisherClient publisherClient = new PublisherClient("cloud-piano", "notes", ServiceAccountCredentialFile);
+            var publisherClient = new Dodgyrabbit.Google.Cloud.PubSub.V1.PublisherClient("", "", ServiceAccountCredentialFile);
             PubSubPublishParameters parameters = new PubSubPublishParameters();
             parameters.Messages = new List<PubSubMessage>();
 
@@ -30,7 +78,6 @@ namespace midi_cloud
                 pubSubMessage.Data = Convert.ToBase64String(Encoding.UTF8.GetBytes($"Message={i}"));
                 parameters.Messages.Add(pubSubMessage);
             }
-
             await publisherClient.PublishAsync(parameters);
         }
     }
